0f49845e7fa44bec9bdf594142abf1d3a7a7ed2f
albertBarsegyan <albertbarsegyan6@gmail.com>
2022-03-22 13:39:08 +0400

    feat(cors.ts): change cors config

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -2,10 +2,11 @@ import cors from "cors";
 
 const allowedOrigins = [
   "http://localhost:3005",
-  "https://todo-applicationapi.herokuapp.com",
+  "https://todo-web-teal.vercel.app",
 ];
 
 export const corsOptions: cors.CorsOptions = {
   origin: allowedOrigins,
   credentials: true,
+  optionsSuccessStatus: 200,
 };
