66e3e335a1fb51dbb9538de8ab0baa9bac8ba0db
albertBarsegyan <albertbarsegyan6@gmail.com>
2022-03-22 11:53:07 +0400

    feat(cors.ts): change cors config

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -1,6 +1,9 @@
 import cors from "cors";
 
-const allowedOrigins = ["http://localhost:3005"];
+const allowedOrigins = [
+  "http://localhost:3005",
+  "https://todo-applicationapi.herokuapp.com",
+];
 
 export const corsOptions: cors.CorsOptions = {
   origin: allowedOrigins,
