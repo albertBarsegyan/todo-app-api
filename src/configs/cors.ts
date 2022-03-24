060d573fe174e64ba7604fcb8c449ba36b14df1a
albertBarsegyan <albertbarsegyan6@gmail.com>
2022-03-24 17:33:59 +0400

    feat(cors.ts): add new cors

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -2,11 +2,10 @@ import cors from "cors";
 
 const allowedOrigins = [
   "http://localhost:3005",
+  "http://localhost:3000",
   "https://todo-web-teal.vercel.app",
 ];
 
 export const corsOptions: cors.CorsOptions = {
   origin: allowedOrigins,
-  credentials: true,
-  optionsSuccessStatus: 200,
 };
